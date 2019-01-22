d0f3a7cae15f9949eacf8a646f8d00b33ec00918
Ripster47 <mpolinski12@gmail.com>
2019-01-21 19:31:14 -0600

    partials finalized

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,19 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def full_name
+    if first_name == "Smitty" && last_name == "Werbenjagermanjensen"
+      "This contact's name is #{first_name} #{last_name}, and HE WAS NUMBER 1!" 
+    else
+      "This contact's name is #{first_name} #{last_name}." 
+    end
+  end
+
+  def japan_phone
+    "+81 #{phone_number}"
+  end
+
 end
