72e52a2533b64ebdae244f10e22db759038d51c3
Ripster47 <mpolinski12@gmail.com>
2019-02-01 12:39:47 -0600

    assigned groups to all contacts

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# ContactGroup.destroy_all
+
+# contacts = Contact.all
+# group_ids = Group.pluck(:id)
+
+# contacts.each do |contact|
+#  selected_group_ids = group_ids.sample(rand(2..4))
+
+#  selected_group_ids.each do |group_id|
+#    ContactGroup.create(contact_id: contact.id, group_id: group_id )
+#  end
+# end
+    
